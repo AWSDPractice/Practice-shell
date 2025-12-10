e06dafb479f8b88c0af5027a8578daea428db9ee
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-10 17:06:37 +0530

    variables level conservation added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=prasanna
+PERSON2=vineela
+
+echo "PERSON1::hi baby"
+echo "PERSON2::HI baby nv yela vunnav"
+echo "PERSON1:: bagane vunna baby nv yem chestunnav"
+echo "PERSON2::netho bratike kshanlu malli kavaali ani korukuntunna"
