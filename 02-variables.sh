c71d5b14b8718f91de47e75b72bb48672e395a61
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-10 16:44:33 +0530

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "prasanna:: hi baby"
+echo "vineela::hi baby yela vunnav"
+echo "prasanna:: bagane vunna baby nv yem chestunnav"
+echo "vineela::netho bratike kshanlu malli kavaali ani korukuntunna"
