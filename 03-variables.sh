db707e44f2068b42f130355faf48e5399303610f
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-10 17:31:33 +0530

    edits done in sccript

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=prasanna
 PERSON2=vineela
 
-echo "PERSON1::hi baby"
-echo "PERSON2::HI baby nv yela vunnav"
-echo "PERSON1:: bagane vunna baby nv yem chestunnav"
-echo "PERSON2::netho bratike kshanlu malli kavaali ani korukuntunna"
+echo (PERSON1)::"hi baby"
+echo (PERSON2)::"hi baby nv yela vunnav"
+echo (PERSON1)::"nv amma dairylo konni pagelu book chaduvu"
+echo (PERSON2)::"sare chaduvuta "
