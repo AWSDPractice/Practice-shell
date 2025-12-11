780b53d601177f6cc07f10a2c50e943d596015fa
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-11 09:43:13 +0530

    variables added to conversation

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,5 +5,5 @@ PERSON2=vineela
 
 echo "$PERSON1::hi baby"
 echo "$PERSON2::Hi baby yela vunnav"
-echo "$PERSON1::nenu baagane vunna nv yem chestunnav
+echo "$PERSON1::nenu baagane vunna nv yem chestunnav"
 echo "$PERSON2::nenu devops nerchukuntunna"
