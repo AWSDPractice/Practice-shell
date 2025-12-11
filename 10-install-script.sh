c58cfd3b5ddf9ed381a14ea416ec8158ed2e5c0d
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-11 22:50:43 +0530

    isntall packages added to script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,4 +10,20 @@ fi
 
 dnf install mysql -y  
 
+if [ $? -gt 0 ]
+then
+    echo "installing mysql ....FAILURE"
+    exit 1
+else 
+    echo "installing mysql...Success"
+fi
+
 dnf install git -y
+
+if [$? -ne 0 ]
+then 
+    echo "installing git ....FAILURE"
+    exit 1
+else
+    echo "installing mysql...SUCCESS"
+fi  
