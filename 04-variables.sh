6fae4d14a431fe187e01a1c0eef6727934343ee3
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-11 09:59:18 +0530

    args added to conversation

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::hi baby"
+echo "$PERSON2::Hi baby yela vunnav"
+echo "$PERSON1::nenu baagane vunna nv yem chestunnav"
+echo "$PERSON2::nenu devops nerchukuntunna"
