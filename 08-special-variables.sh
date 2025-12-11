eeb3bd5674a113919593044b4caf183c75f73126
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-11 15:45:22 +0530

    special variables added to script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -4,9 +4,9 @@ echo "special variables: $1, $2, $3"
 echo "All variables: $@"
 echo "no.of variables: $#"
 echo "Script name: $0"
-echo "Present working directory: $pwd"
+echo "Present working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "process id of current script : $$"
-sleep 60 
+sleep 60 &
 echo "process id of last command in background: $!"
