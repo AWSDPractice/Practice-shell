90d32e1239466a138df9c5140cc6b8c4d4f77587
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-15 11:20:18 +0530

    pks installing through function

---
+++ 11-functions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+ 
+ USERID=$(id -u)
+
+VALIDATE(){
+
+     if [ $1 -gt 0 ]
+    then
+        echo "$2 ....FAILURE"
+        exit 1
+    else 
+        echo "$2..Success"
+    fi
+
+
+}
+
+ if [ $USERID -gt 0 ]
+ then
+    echo "ERROR ::You must have sudo access to execute this script"
+    exit 1 # other than 0
+fi 
+
+dnf list installed mysql
+
+if [$? -gt 0]
+then
+    dnf install mysql -y  
+    VALIDATE $?
+else
+    echo "MYSQL is already installed"
+fi
+
+dnf list installed git
+
+if [ $? -gt 0]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+   
+else
+    echo " git is already installed"
+fi  
