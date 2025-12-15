b9bfa0c088f558c1fe952d64df059a544334f5bb
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-15 11:44:14 +0530

    colours added to functions

---
+++ 12-colors.sh
@@ -0,0 +1,46 @@
+#1/bin/bash
+
+ USERID=$(id -u)
+ R="\e[31m"
+ G="\e[32m"
+ y="\e[33m"
+
+
+VALIDATE(){
+
+     if [ $1 -gt 0 ]
+    then
+        echo -e "$2 ....$R FAILURE"
+        exit 1
+    else 
+        echo -e "$2..$G Success"
+    fi
+
+}
+
+ if [ $USERID -gt 0 ]
+ then
+    echo "ERROR ::You must have sudo access to execute this script"
+    exit 1 # other than 0
+fi 
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y  
+    VALIDATE $?
+else
+    echo -e "$Y MYSQL is already installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+   
+else
+    echo -e "$Y git is already installed"
+fi  
