28ef1fe361adbe57cc713fda2564dfdc77727e23
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-18 21:10:01 +0530

    while condition added to old logs

--- 16-delete-oldlogs.sh
+++ 16-delete-oldlogs.sh
@@ -25,7 +25,6 @@ VALIDATE(){
     else 
         echo -e "$2 .... ${G}SUCCESS${N}"
     fi
-
 }
 
  if [ $USERID -ne 0 ]
@@ -34,8 +33,13 @@ VALIDATE(){
     exit 1 # other than 0
 fi 
 
-
 echo "script started at: $TIME_STAMP"  &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "Files to be deleted:$FILES_TO_DELETE
+echo "Files to be deleted: $FILES_TO_DELETE
+
+while read -r file
+do
+    echo "deleting file: $file"
+
+done <<< $FILES_TO_DELETE
