bd425c5cd360bd7086c4923b2aabcecd27ee17c9
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-19 16:03:03 +0530

    backup script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -12,7 +12,7 @@ SOURCE_DIR=$1
 DES_DIR=$2
 DAYS=${3: -14} #if user not providing no.of datys we are taking 14 days as default
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
  LOG_FILE=$(echo $0 | cut -d "." -f1)
  TIME_STAMP=$(date +%Y-%m-%d-%H-%M)
  LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIME_STAMP.log"
