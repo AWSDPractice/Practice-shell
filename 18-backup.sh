c2b78d028963a822baa51ac2ffae900245467f86
vineela2025 <vineelakommojudevops@gmail.com>
2025-12-19 17:04:30 +0530

    backup script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -10,7 +10,7 @@
 
 SOURCE_DIR=$1
 DES_DIR=$2
-DAYS=${3: -14} #if user not providing no.of datys we are taking 14 days as default
+DAYS=${3:-14} #if user not providing no.of datys we are taking 14 days as default
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
  LOG_FILE=$(echo $0 | cut -d "." -f1)
@@ -18,7 +18,7 @@ LOGS_FOLDER="/home/ec2-user/shellscript-logs"
  LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIME_STAMP.log"
 
 #creating log directory
-mkdir -p /home/ec2-user/shellscript-logs/
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
 
@@ -36,7 +36,6 @@ USAGE(){
     exit 1
 }
 
-mkdir -p /home/ec2-user/shellscript-logs/
 
 if [ $# -lt 2 ]
 then
@@ -49,7 +48,7 @@ then
     exit 1
 fi
 
-if [ ! -d DES_DIR ]
+if [ ! -d $DES_DIR ]
 then
     echo -e "$DES_DIR does not exist...please check"
     exit -1
